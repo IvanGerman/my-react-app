0730a007379ac6e5901cd0ad7814e64c78d7215f
IvanGerman <ivgvi@mail.ru>
2020-08-11 15:20:00 +0200

    Deep/Shallow Copy

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { updateNewMessageBodyCreator, sendMessageCreator } from '../../redux/dialogs-reducer';
 import Dialogs from './Dialogs';
+import { connect } from 'react-redux';
 
 
-
-
-const DialogsContainer = (props) => {
-    
-    let state = props.store.getState().dialogsPage;
-     
-
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator());
-    };
-    let onNewMessageChange = (body) => {
-        props.store.dispatch(updateNewMessageBodyCreator(body));
-    };
-
-    return (
-        <Dialogs updateNewMessageBody = {onNewMessageChange} sendMessage = {onSendMessageClick} dialogsPage = {state} />
-    )
+const mapStateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
+    }
 };
-
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewMessageBody: (body) => {
+            dispatch(updateNewMessageBodyCreator(body));
+        },
+        sendMessage: () => {
+            dispatch(sendMessageCreator());
+        }
+    }
+}
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps) (Dialogs);
 export default DialogsContainer;
--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -22,14 +22,25 @@ let initialState = {
 
 const dialogsReducer = (state = initialState, action) => {
     switch(action.type) {
-        case UPDATE_NEW_MESSAGE_BODY: 
-            state.newMessageBody = action.body;
-            return state;
-        case SEND_MESSAGE:
-            let body = state.newMessageBody;
-            state.messages.push( { id: 6,message: body } );
-            state.newMessageBody = "";
-            return state;
+        case UPDATE_NEW_MESSAGE_BODY: {
+            let stateCopy = {...state};
+            stateCopy.newMessageBody = action.body;
+            return stateCopy;
+            //state.newMessageBody = action.body;
+            //return state;
+          }
+        case SEND_MESSAGE: {
+            let stateCopy = {...state};
+            let body = stateCopy.newMessageBody;
+            stateCopy.messages = [...state.messages];
+            stateCopy.messages.push( { id: 6,message: body } );
+            stateCopy.newMessageBody = "";
+            return stateCopy;
+            //let body = state.newMessageBody;
+            //state.messages.push( { id: 6,message: body } );
+            //state.newMessageBody = "";
+            //return state;
+          }
         default: 
             return state;
     };
