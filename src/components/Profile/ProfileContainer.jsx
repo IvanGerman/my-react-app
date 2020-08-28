b4b1e7269522b707b24a768e2f005e266ae28d11
IvanGerman <ivgvi@mail.ru>
2020-08-28 15:29:48 +0200

    bevor login/auth me

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -9,8 +9,12 @@ import { withRouter } from 'react-router-dom';
 class ProfileContainer extends React.Component {
 
     componentDidMount () {
+      let userId = this.props.match.params.userId;
+      if (!userId) {
+        userId = 2;
+      };
       
-      axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`).then(response => {
+      axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId).then(response => { 
           this.props.setUserProfile(response.data);
       });
     }
@@ -26,5 +30,5 @@ let mapStateToProps = (state) => ({
   profile: state.profilePage.profile
 });
 
-//let WithUrlDataContainerComponent = withRouter(ProfileContainer);
-export default connect(mapStateToProps, {setUserProfile}) (ProfileContainer);
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
+export default connect(mapStateToProps, {setUserProfile}) (WithUrlDataContainerComponent);
