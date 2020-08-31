98f9efd75c40a4794425de12a399f252be8c1d11
IvanGerman <ivgvi@mail.ru>
2020-08-31 16:47:12 +0200

    DAL,axious.create

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import * as axios from 'axios';
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL : `https://social-network.samuraijs.com/api/1.0/`,
+    headers: { "API-KEY": "1371f8f6-2f45-496f-8d3b-e435c469f3e2"}
+})
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 5)  { 
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+                    .then(response => {
+                        return response.data;
+                    });
+        
+        }
+}
+
